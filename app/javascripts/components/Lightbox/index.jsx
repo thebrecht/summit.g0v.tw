1291bb507a202e86a86e43f5ee4e40cbb4580df0
shelling <navyblueshellingford@gmail.com>
2016-04-07 10:41:32 +0800

    Add Lightbox component

---
+++ app/javascripts/components/Lightbox/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { getLocale } from "javascripts/locale";
+import styles from "./styles.css";
+
+class Lightbox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render() {
+  }
+};
+
+export default Lightbox;
